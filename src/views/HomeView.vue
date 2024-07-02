7fc8137e0fd9ca3eb3420a1358fd6e3c4e302afc
кαтнєєѕкυмαɾ <katheeskumar@outlook.com>
2024-07-02 10:26:13 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <main class="wrapperx">
+      <h1>(Zero) Parallel Utility Line IOT</h1>
+      <small>
+        Think Smart Run with Zero Negativity
+      </small>
+  </main>
+</template>
+
+<style>
+.wrapperx{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
